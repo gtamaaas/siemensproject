bd10eed882252104bfef7c48acbfdc634991c3cd
gtamaaas <gaspar.tamas315@gmail.com>
2024-05-15 18:02:36 +0300

    Added default message when radius is not big enough

--- SiemensProjectFrontEnd/src/Searchbar.jsx
+++ SiemensProjectFrontEnd/src/Searchbar.jsx
@@ -3,21 +3,25 @@ import { hotels } from "./hotels";
 
 function SearchBar() {
   const [searchInput, setSearchInput] = useState("");
+  const filteredHotels = hotels.filter(
+    (hotel) => hotel.distance <= searchInput
+  );
+
   return (
     <>
       <input
         placeholder="Enter desired radius"
         onChange={(event) => setSearchInput(event.target.value)}
       />
-      {hotels
-        .filter((hotel) => {
-          if (hotel.distance <= searchInput) return hotel;
-        })
-        .map((hotel) => (
-          <div>
+      {filteredHotels.length > 0 ? (
+        filteredHotels.map((hotel) => (
+          <div key={hotel.id}>
             {hotel.name} {hotel.distance}
           </div>
-        ))}
+        ))
+      ) : (
+        <div>No hotels found within the specified radius.</div>
+      )}
     </>
   );
 }
