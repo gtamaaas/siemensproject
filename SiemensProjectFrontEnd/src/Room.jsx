7be4900f47d7855fdae716e1f89da0345013ce6e
gtamaaas <gaspar.tamas315@gmail.com>
2024-05-15 22:58:47 +0300

    Toggling and filtering logic

--- SiemensProjectFrontEnd/src/Room.jsx
+++ SiemensProjectFrontEnd/src/Room.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
 
-function Room({ room }) {
+function Room({ room, toggledRooms, setToggledRooms }) {
   const [checked, setChecked] = useState(false);
 
   const handleChange = () => {
     setChecked(!checked);
+    if (toggledRooms.indexOf(room.roomNumber) == -1) {
+      setToggledRooms([...toggledRooms, room.roomNumber]);
+    } else {
+      setToggledRooms(
+        toggledRooms.filter((toggledRoom) => toggledRoom !== room.roomNumber)
+      );
+    }
   };
   return (
     <div>
