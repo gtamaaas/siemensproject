c0e18b3a6d654eabfe4d37f9876055224cbe6d2e
gtamaaas <gaspar.tamas315@gmail.com>
2024-05-16 15:38:30 +0300

    Added backend skeleton

---
+++ SiemensProjectBackEnd/src/main/java/com/example/siemensprojectbackend/SiemensProjectBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.siemensprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiemensProjectBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiemensProjectBackEndApplication.class, args);
+    }
+
+}
---
+++ SiemensProjectBackEnd/src/test/java/com/example/siemensprojectbackend/SiemensProjectBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.siemensprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiemensProjectBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
