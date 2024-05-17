5b916e2bee0e34a38c97f21043c2c86fe791fc33
gtamaaas <gaspar.tamas315@gmail.com>
2024-05-17 18:25:24 +0300

    Wrapper class for coordinates

---
+++ SiemensProjectBackEnd/src/main/java/com/example/siemensprojectbackend/model/Coordinates.java
@@ -0,0 +1,30 @@
+package com.example.siemensprojectbackend.model;
+
+public class Coordinates {
+    private double longitude;
+    private double latitude;
+
+    public Coordinates() {
+    }
+
+    public Coordinates(double longitude, double latitude) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+}
