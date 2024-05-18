28eb1611a057678b6f17821d4a523d12aeee3601
gtamaaas <gaspar.tamas315@gmail.com>
2024-05-18 12:55:18 +0300

    Updated design

--- SiemensProjectFrontEnd/src/HotelItem.jsx
+++ SiemensProjectFrontEnd/src/HotelItem.jsx
@@ -2,12 +2,22 @@ import { Link } from "react-router-dom";
 
 function HotelItem({ hotel }) {
   const id = hotel.id;
+  const distance = String(hotel.distance);
   return (
     <>
-      <div key={hotel.id} class="flex gap-10 border">
-        <div>{hotel.name}</div>
-        <div> Distance from you: {hotel.distance}</div>
-        <Link to={`hotels/` + id}>Check out hotel</Link>
+      <div
+        key={hotel.id}
+        class="flex gap-10 justify-between border flex-col  w-96"
+      >
+        <div className="flex">
+          <div className="flex-1">{hotel.name}</div>
+          <div className=""> Distance: {distance.substring(0, 5)}</div>
+        </div>
+        <div className="flex justify-center">
+          <Link className="bg-blue-100 rounded-3xl p-3 m-3" to={`hotels/` + id}>
+            Check out hotel
+          </Link>
+        </div>
       </div>
     </>
   );
--- SiemensProjectFrontEnd/src/Searchbar.jsx
+++ SiemensProjectFrontEnd/src/Searchbar.jsx
@@ -8,16 +8,19 @@ function SearchBar({ hotels }) {
   );
 
   return (
-    <div>
-      <input
-        placeholder="Enter desired radius"
-        onChange={(event) => setSearchInput(event.target.value)}
-      />
-      {filteredHotels.length > 0 ? (
-        filteredHotels.map((hotel) => <HotelItem hotel={hotel} />)
-      ) : (
-        <div>No hotels found within the specified radius.</div>
-      )}
+    <div className="flex justify-center items-center h-screen">
+      <div className="bg-red-100 flex flex-col">
+        <input
+          className="text-center"
+          placeholder="Enter desired radius"
+          onChange={(event) => setSearchInput(event.target.value)}
+        />
+        {filteredHotels.length > 0 ? (
+          filteredHotels.map((hotel) => <HotelItem hotel={hotel} />)
+        ) : (
+          <div>No hotels found within the specified radius.</div>
+        )}
+      </div>
     </div>
   );
 }
