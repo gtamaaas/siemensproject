1eb21999af7a536ddca9fe8adae2dae87172cc87
gtamaaas <gaspar.tamas315@gmail.com>
2024-05-18 11:02:19 +0300

    New method to find reservations by hotelid

--- SiemensProjectBackEnd/src/main/java/com/example/siemensprojectbackend/repository/ReservationRepository.java
+++ SiemensProjectBackEnd/src/main/java/com/example/siemensprojectbackend/repository/ReservationRepository.java
@@ -3,5 +3,8 @@ package com.example.siemensprojectbackend.repository;
 import com.example.siemensprojectbackend.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    public List<Reservation> findAllByHotelId(Long id);
 }
